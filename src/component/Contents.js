d29d4005c72cfde2c11bc9f8ed6536d7544edcfd
unknown <rnrudxo2872@naver.com>
2021-03-09 00:31:07 +0900

    json list화 완료

---
+++ src/component/Contents.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Contents extends Component{
+  
+    render(){
+        return(
+            <h1></h1>
+        );
+    }
+}
+
+export default Contents;
--- src/component/TopOfPage.js
+++ src/component/TopOfPage.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import './TopOfPage.css';
 
 class TopOfPage extends Component{
     render(){ // 현 javascript 스펙에서는 class 안에서 function은 function을 생략
       return(
-        <h1>Hello, React Tutorial</h1>
+        <h1>{this.props.sub.title}</h1>
       );
     }
 }
